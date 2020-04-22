39b87b57fdbbd9b49004e4e1f72194643d1762d7
Denis Konovalyenko <denis.konovalyenko@gmail.com>
2020-04-22 18:08:55 +0300

    #5: Releasing from master branch.

--- ci/scripts/sonatype-release.sh
+++ ci/scripts/sonatype-release.sh
@@ -10,10 +10,9 @@ CI_PATH="ci"
 CI_SCRIPTS_PATH="${CI_PATH}/scripts"
 MAVEN_CLI_OPTS="--settings ${CI_PATH}/.m2/settings.xml --batch-mode --errors --show-version --fail-at-end -DinstallAtEnd=true -DdeployAtEnd=true"
 
-git checkout $(git rev-parse HEAD)
+git checkout master
 mvn versions:set "-DnewVersion=${TAG}"
 ${CI_SCRIPTS_PATH}/artifacts-signing-key-decryption.sh
 mvn deploy ${MAVEN_CLI_OPTS} -DskipTests --activate-profiles code-source,code-documentation,artifacts-signing,artifacts-deployment
 git commit -am "Release ${TAG}"
 git tag -f ${TAG}
-
